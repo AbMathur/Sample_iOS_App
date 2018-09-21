f25d550e1e4197da54adbe78d92801e0907ec1de
Centelon <centelon@centelons-macbook-pro.local>
2018-09-21 17:45:57 +0530

    Initial Commit

---
+++ Sample_iOS_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample_iOS_App
+//
+//  Created by Centelon on 21/9/18.
+//  Copyright © 2018 Centelon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
